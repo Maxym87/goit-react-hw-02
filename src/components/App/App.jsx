331ad4301330cde6d3ad4b56fe6a760175e694f8
Maxym87 <112802810+Maxym87@users.noreply.github.com>
2024-04-28 12:32:03 +0300

    add couting function

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,5 +1,24 @@
-
+import { useState } from "react";
+import Description from "../Description/Description";
+import Options from "../Options/Options";
+import Feedbak from "../Feedback/Feedback";
 
 export default function App() {
 
+  const [clicks, setClicks] = useState(0)
+  
+  const updateFeedback = feedbackType => {
+setClicks({...clicks, [feedbackType] : clicks[feedbackType] + 1})
 }
+
+
+    return (
+      <>
+        <Description />
+        <Options onClick={updateFeedback} />
+        <Feedbak value={clicks} />
+      </>
+
+    )
+  }
+
---
+++ src/components/Feedback/Feedback.jsx
@@ -0,0 +1,14 @@
+
+
+
+export default function Feedbak({value}) {
+
+
+  return (
+    <div>
+      <p>Good: {value}</p>
+      <p>Neutral: {value}</p>
+      <p>Bad: {value}</p>
+    </div>
+  )
+}
